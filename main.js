f15aae3f2ab409680978c6ec7ce30b3497f377d3
seungsu-K <kss095@naver.com>
2024-07-10 17:04:40 +0900

    remove : index.html 경로 수정

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ import '@/styles/style.scss';
 import '@/layout/footer';
 import '@/styles/pages/onboarding_scroll_section_1.scss';
 import '@/styles/pages/login_ID.scss';
-import '@/pages/onboarding/11onboarding.js';
 
 // const records = await pb.collection('test').getFullList({
 //   sort: '-created',
