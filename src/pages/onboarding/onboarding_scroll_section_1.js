8e77938b89cafa5f3d765a16255324ed82b6873b
daggi-01 <dhemd1111@gmail.com>
2024-07-10 12:36:01 +0900

    init : scroll_section_1 , login_ID 생성 feat: secroll_section-1 기능 구현, 이슈 수정X, login_ID 마크업

---
+++ src/pages/onboarding/onboarding_scroll_section_1.js
@@ -0,0 +1,41 @@
+import '@/styles/pages/onboarding_scroll_section_1.scss';
+import 'swiper/css/bundle';
+import Swiper from 'swiper/bundle';
+import { Pagination } from 'swiper/modules';
+
+const swiper = new Swiper('.swiper', {
+  autoplay: {
+    delay: 1000,
+  },
+  // lopp: true,
+  speed: 2000,
+  // parallx: true,
+  // // slidesPerView: 1,
+  // // slidesPerGroup: 1,
+  // spaceBetween: 20,
+  // // freeMode: true,
+  // breakpoints: {
+  //   1920: {
+  //     slidesPerView: 1,
+  //     slidesPerGroup: 1,
+  //   },
+  //   768: {
+  //     slidesPerView: 1,
+  //     slidesPerGroup: 1,
+  //   },
+  // },
+});
+
+// const swiper2 = new Swiper('.pc-swiper', {
+//   autoplay: {
+//     delay: 3000,
+//   },
+//   lopp: true,
+//   seepd: 2000,
+//   parallx: true,
+// });
+/*
+css 로 만들면 안된다 
+
+
+*/
