32a7b0d66e784f203175f0eb728ba27591fb156a
seungsu-K <kss095@naver.com>
2024-07-10 13:25:29 +0900

    feat : renderImgList 이미지 랜더링 기능

--- src/library/renderImgList.js
+++ src/library/renderImgList.js
@@ -1,10 +1,8 @@
 import { insertLast } from '@/library/insert';
 import getPbImageURL from '@/api/getPbImageURL';
-import pb from '@/api/pocketbase';
 
 export async function renderImgList(storage, node) {
-  const records = await pb.collection(storage).getFullList();
-
+  const records = await storage;
   records.forEach((item) => {
     const template = `
       <li class="swiper-slide">
@@ -15,5 +13,39 @@ export async function renderImgList(storage, node) {
       </li>
     `;
     insertLast(node, template);
+  });
+}
+
+export async function renderImgTitleList(storage, node) {
+  const records = await storage;
+  records.forEach((item) => {
+    if (Object.prototype.hasOwnProperty.call(item, 'episode')) {
+      const template = `
+      <li class="swiper-slide">
+        <img
+          src="${getPbImageURL(item)}"
+          alt="${item.title}"
+        />
+        <h3>${item.title}</h3>
+        <span>${item.episode}</span>
+      </li>
+      
+    `;
+      insertLast(node, template);
+    } else {
+      const template = `
+      <li class="swiper-slide">
+        <img
+          src="${getPbImageURL(item)}"
+          alt="${item.title}"
+        />
+        <h3>
+          <span>${records.indexOf(item) + 1}</span>
+          <span>${item.title}</span>
+        </h3>
+      </li>
+    `;
+      insertLast(node, template);
+    }
   });
 }
