9de387a60c4d8394dcf65a4aad94235e56f7c324
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2024-07-10 22:28:47 +0900

    test : 배포5(폴더 정리)

--- src/pages/onboarding/onboarding.js
+++ src/pages/onboarding/onboarding.js
@@ -2,3 +2,4 @@ import '@/pages/onboarding/onboarding_header.js'
 import '@/pages/onboarding/onboarding_section1.js'
 import '@/pages/onboarding/onboarding_section2.js'
 import '@/pages/onboarding/onboarding_footer.js'
+
--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,6 @@ export default defineConfig({
     rollupOptions: {
       input: {
         home: resolve(__dirname, 'index.html'),
-        main: resolve(__dirname, 'src/pages/main/index.html'),
 
         // page 구현 시 연결할 html 파일
         // register: resolve(__dirname, 'src/pages/register/index.html'),
