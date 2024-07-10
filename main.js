a1e6999e9e4e15900c8032e02d9393e57b24a06e
daggi-01 <dhemd1111@gmail.com>
2024-07-10 12:29:41 +0900

    feat : index HTNL 링크추가, main JS 링크추가

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import '@/styles/style.scss';
 // import pb from '@/api/pocketbase';
 import '@/layout/footer';
+import '@/src/onboarding/onboarding_scroll_section_1';
+import '@/src/loginID/login_ID';
 
 // const records = await pb.collection('test').getFullList({
 //   sort: '-created',
