32ad86a8690eabcc0bc561e40f826410c07d4aee
ksh17 <rkstkd98@naver.com>
2024-07-17 03:48:14 +0900

    feature : 로그인 유저 이미지 랜더링 코드 작성

--- src/layout/profile_item.js
+++ src/layout/profile_item.js
@@ -1,7 +1,25 @@
+import getPbImageURL from '../api/getPbImageURL';
 import { insertLast } from '../library/insert';
 
 export function renderProfileItem(pageName) {
   console.log(pageName);
+  const userData = JSON.parse(localStorage.getItem('user'));
+  if (!userData) {
+    location.href = '/src/pages/loginID/';
+    return;
+  }
+  const { record } = userData;
+  const newItem = {
+    ...record,
+    avatar: getPbImageURL(
+      {
+        ...record,
+        avatar: [record.avatar],
+      },
+      'mobile',
+      'avatar'
+    ),
+  };
 
   //임시 이미지
   const sbImg =
@@ -10,26 +28,23 @@ export function renderProfileItem(pageName) {
     'https://yooniverse.pockethost.io/api/files/gym9pmfmkdf9wtq/s32k8bhcyayhd4d/profile_3_desktop_BHKRAb138M.png?token=';
   const ymImg =
     'https://yooniverse.pockethost.io/api/files/gym9pmfmkdf9wtq/dcbodtzpvpt5ulf/profile_2_desktop_ujnfC7hxCb.png?token=';
-  const edImg =
-    'https://yooniverse.pockethost.io/api/files/gym9pmfmkdf9wtq/5r0tzi5c3f1xq0s/profile_4_desktop_UuxTGhRfov.jpg?token=';
 
   const profiles = [
-    { name: '슬비님', imgSrc: sbImg },
-    { name: '범쌤', imgSrc: bsImg },
-    { name: '야무', imgSrc: ymImg },
-    { name: '이듬', imgSrc: edImg },
+    { name: '슬비님', avatar: sbImg },
+    { name: '범쌤', avatar: bsImg },
+    { name: '야무', avatar: ymImg },
   ];
 
-  profiles.forEach((item) => {
+  [...profiles, newItem].forEach((item) => {
     const template = `    <figure class="avatar">
-          <div class="edit__icon__container">
+          <a href="/src/pages/${pageName === 'selectPage' ? 'taing' : 'register'}/index.html" class="edit__icon__container">
             <svg class="edit-icon" role="img" aria-label="편집 아이콘">
               <use href="/icons/stack.svg#${pageName === 'selectPage' ? 'profile-lock' : 'profile-pencil'}" />
             </svg>
-          </div>
+          </a>
           <img
             class="avatar__picture"
-            src=${item.imgSrc}
+            src=${item.avatar || sbImg}
             alt="프로필 아바타"
           />
           <figcaption class="avatar__name">${item.name}</figcaption>
