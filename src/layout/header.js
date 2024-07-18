ecbc56aff9b3790b80c2a1e1cfd3686e4a0148bd
seungsu-K <kss095@naver.com>
2024-07-19 01:07:26 +0900

    modify : 헤더 프로필

--- src/layout/header.js
+++ src/layout/header.js
@@ -2,12 +2,17 @@ import pb from '@/api/pocketbase';
 import { getStorage, deleteStorage } from '@/library/index';
 import '@/styles/layout/search_modal.scss';
 import textCSS from '@/styles/layout/header.scss?inline';
-
-// 기본값
-// setStorage('auth', defaultAuthData)
+import getPbImageURL from '@/api/getPbImageURL';
 
 const headerTemplate = document.createElement('template');
-headerTemplate.innerHTML = `
+
+async function setUserDataOnTemplate() {
+  const user = await getStorage('user');
+  console.log(user);
+
+  // const { collectionId, username, id, avatar } = user.record;
+
+  headerTemplate.innerHTML = `
   <style>
     ${textCSS}
   </style>
@@ -109,7 +114,7 @@ headerTemplate.innerHTML = `
                 </li>
                 <li class="popular__list__order"><span>10</span>술꾼도시여자들</li>
               </ul>
-            
+
               <div class="date">2024.07.14 오후 10시 41 기준</div>
               <!-- 날짜 -->
             </div>
@@ -119,13 +124,13 @@ headerTemplate.innerHTML = `
         </div>
         <!-- 프로필 --------------------------------------->
           <button type="button" class="button_profile_open">
-            <img src="/image/profile_4.png" alt="프로필 이동하기" />
+            <img src="https://yooniverse.pockethost.io/api/files/${user.record.collectionId}/${user.record.id}/${user.record.avatar}" alt="프로필 이동하기" />
           </button>
           <div class="profile">
             <div class="profile_wrapper">
               <div class="profile_container">
-                <img src="/image/profile_4.png" alt="프로필" />
-                <h2>이듬</h2>
+                <img src="https://yooniverse.pockethost.io/api/files/${user.record.collectionId}/${user.record.id}/${user.record.avatar}" alt="프로필" />
+                <h2>${user.record.username}</h2>
                 <button type="button" onclick="location.href='/src/pages/profile_edit_detail/index.html'">
                   <span>프로필 편집</span>
                 </button>
@@ -145,9 +150,157 @@ headerTemplate.innerHTML = `
             </div>
           </div>
         </div>
-      </div>
-    </nav>
-`;
+        </div>
+        </nav>
+  `;
+
+  console.log(headerTemplate.content);
+}
+
+// setUserDataOnTemplate();
+// console.log(headerTemplate.content);
+
+// headerTemplate.innerHTML = `
+//   <style>
+//     ${textCSS}
+//   </style>
+//   <header class="header">
+//     <nav class="nav">
+//       <h1 class="header__logo">
+//         <a class="header__logo__link" href="/index.html">
+//           <svg class="logo1" role="img" aria-label="타잉">
+//             <use href="/icons/stack.svg#logo" />
+//           </svg>
+//         </a>
+//       </h1>
+//       <ul class="header__menu">
+//         <li class="menu__list">
+//           <a class="list__live" href="/">
+//             <span aria-label="라이브 페이지로 이동하기">실시간</span>
+//           </a>
+//         </li>
+//         <li class="menu__list">
+//           <a class="list__tv" href="/">
+//             <span>TV프로그램</span>
+//           </a>
+//         </li>
+//         <li class="menu__list">
+//           <a class="list__movie" href="/">
+//             <span>영화</span>
+//           </a>
+//         </li>
+//         <li class="menu__list">
+//           <a class="list__paramount" href="/">
+//             <span aria-label="파라마운트 이동하기"></span>
+//           </a>
+//         </li>
+//       </ul>
+//       <div class="header__actions">
+//         <!-- 검색창 --------------------------------------->
+//         <button type="button" class="button_search_open" aria-label="검색창으로 이동하기">
+//         </button>
+//         <div class="search">
+//           <div class="search__wrapper">
+//             <form class="search__bar" action="URL" method="POST">
+//             <input
+//               class="search__bar__input"
+//               id="search__box1"
+//               type="text"
+//               name="searchBox"
+//               placeholder="검색"
+//               required
+//               aria-invalid="false"
+//               aria-live="polite"
+//             />
+//             <input
+//               class="search__bar__input2"
+//               id="search__box2"
+//               type="text"
+//               name="searchBox"
+//               placeholder="TV프로그램, 영화 제목 및 출연진으로 검색해보세요"
+//               required
+//               aria-invalid="false"
+//               aria-live="polite"
+//             />
+//             <button type="button" class="search__icon__button">
+//               <svg class="search__icon" role="img" aria-label="검색 아이콘">
+//                 <use href="/icons/stack.svg#search-defualt" />
+//               </svg>
+//             </button>
+//                     </form>
+//                     <!-- 검색 바 -->
+//                     <section class="search__list__container">
+//             <div class="recent__search">
+//               <h2>최근 검색어</h2>
+//               <ul id="recent__search__list">
+//                 <li>검색 내역이 없습니다</li>
+//               </ul>
+//               <button type="button" id="recent__delet__button"></button>
+//             </div>
+//             <!-- 최근 검색어 -->
+//             <div class="popular__search">
+//               <h2>실시간 인기 검색어</h2>
+//               <ul class="popular__list">
+//                 <li class="popular__list__order"><span>1</span>재벌집 막내아들</li>
+//                 <li class="popular__list__order">
+//                   <span>2</span>미드터트롯2: 새로운 전설의 시작
+//                 </li>
+//                 <li class="popular__list__order">
+//                   <span>3</span>유 퀴즈 온 더 블럭
+//                 </li>
+//                 <li class="popular__list__order"><span>4</span>대행사</li>
+//                 <li class="popular__list__order">
+//                   <span>5</span>SHOW MW THE MONEY 11
+//                 </li>
+//                 <li class="popular__list__order">
+//                   <span>6</span>미씽: 그들이 있었다2
+//                 </li>
+//                 <li class="popular__list__order"><span>7</span>술꾼도시여자들2</li>
+//                 <li class="popular__list__order"><span>8</span>캐나다 체크인</li>
+//                 <li class="popular__list__order">
+//                   <span>9</span>미씽:그들이 있었다 - 그들을 다만나다
+//                 </li>
+//                 <li class="popular__list__order"><span>10</span>술꾼도시여자들</li>
+//               </ul>
+
+//               <div class="date">2024.07.14 오후 10시 41 기준</div>
+//               <!-- 날짜 -->
+//             </div>
+//             <!-- 실시간 인기 검색어 -->
+//                     </section>
+//           </div>
+//         </div>
+//         <!-- 프로필 --------------------------------------->
+//           <button type="button" class="button_profile_open">
+//             <img src="/image/profile_4.png" alt="프로필 이동하기" />
+//           </button>
+//           <div class="profile">
+//             <div class="profile_wrapper">
+//               <div class="profile_container">
+//                 <img src="/icons/icon.svg" alt="프로필" />
+//                 <h2>이듬</h2>
+//                 <button type="button" onclick="location.href='/src/pages/profile_edit_detail/index.html'">
+//                   <span>프로필 편집</span>
+//                 </button>
+//               </div>
+//               <div class="button_container">
+//                 <button type="button" class="contents_button">
+//                   <img src="/image/icon_TV.svg" alt="티비 아이콘" />
+//                   <span>시청 중인 컨텐츠</span>
+//                 </button>
+//                 <button type="button" class="logout_button">
+//                   <img src="/image/icon_logout.svg" alt="로그아웃 아이콘" />
+//                   <span>로그아웃</span>
+//                 </button>
+//                 <button type="button" class="deleteID_button">
+//                   <span>회원탈퇴</span>
+//                 </button>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </nav>
+// `;
 
 // if (!localStorage.getItem('auth')) {
 //   setStorage('auth', defaultAuthData);
@@ -161,9 +314,16 @@ export class Header extends HTMLElement {
   }
 }
 
-customElements.define('c-header', Header);
+(async function () {
+  await setUserDataOnTemplate();
+  await customElements.define('c-header', Header);
+})();
 
 const cHeader = document.querySelector('c-header');
+
+console.log(cHeader);
+console.log(cHeader.shadowRoot);
+
 const headerMenu = cHeader.shadowRoot.querySelector('.header__menu');
 const buttonSearch = cHeader.shadowRoot.querySelector('.button_search_open');
 const buttonProfile = cHeader.shadowRoot.querySelector('.button_profile_open');
