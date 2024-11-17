342011ad744d684a0583ce85a2f4aa0cdd5733f5
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2024-11-17 23:33:58 +0900

    feat: renderImgList-media속성 수정

--- src/library/renderImgList.js
+++ src/library/renderImgList.js
@@ -9,9 +9,9 @@ export async function renderImgList(collection, categoryName, node) {
     const template = `
       <li class="swiper-slide">
         <picture>
-          <source srcset="${getPbImageURL(item)}" media="max-width: 768px" />
-          <source srcset="${getPbImageURL(item, 'tablet')}" media="(max-width: 1919px)" />
-          <source srcset="${getPbImageURL(item, 'desktop')}"/>
+          <source srcset="${getPbImageURL(item)}" media="(max-width: 767px)" />
+          <source srcset="${getPbImageURL(item, 'tablet')}" media="(min-width: 768px) and (max-width: 1279px)" />
+          <source srcset="${getPbImageURL(item, 'desktop')}" media="(min-width: 1280px)"/>
           <img src="/img/logo.svg" alt="티빙 로고" />
         </picture>
       </li>
