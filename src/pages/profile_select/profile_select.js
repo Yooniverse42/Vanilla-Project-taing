548abe125b3506eae21a6d6b1acff258f1b408a9
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2024-11-26 23:21:40 +0900

    feat: profile_edit 페이지 지우고 버튼 클릭시 편집이 되도록 구현함

--- src/pages/profile_select/profile_select.js
+++ src/pages/profile_select/profile_select.js
@@ -1,11 +1,44 @@
-import '@/styles/pages/profile.scss';
+import '@//pages/profile_select/profile.scss';
 import '@/layout/footer';
-import { renderProfileItem } from '../../layout/profile_item';
-import { getNode } from '../../library/getNode';
-renderProfileItem('selectPage');
+import { renderProfileItem } from '@/layout/profile_item';
+import { getNode, getNodes } from '@/library/getNode';
+renderProfileItem('main');
 
+const title = getNode('.header__title__container');
 const avatarsContainer = getNode('.profile__picture__container');
-avatarsContainer.addEventListener('click', goToTaing);
+const avatarsPictures = getNodes('.avatar__picture__container');
+const avatarsEditButton = getNode('.edit__button');
+const avatarsAddButton = getNode('.add__button');
+
+// 프로필 편집 (전체 페이지)
+function editPage() {
+  // 기존 프로필 아이템들을 지우고
+  avatarsContainer.innerHTML = '';
+
+  // 편집 모드로 다시 렌더링
+  renderProfileItem('edit');
+
+  title.innerHTML = `프로필 편집
+  <p>편집할 프로필을 선택해 주세요.</p>`;
+
+  avatarsPictures.forEach((avatar) => {
+    if (avatar.classList.contains('is--locked')) {
+      avatar.classList.remove('is--locked');
+      avatar.classList.add('is--edit');
+    } else {
+      avatar.classList.add('is--edit');
+    }
+  });
+}
+avatarsEditButton.addEventListener('click', editPage);
+
+// 프로필 추가
+function addAvatar() {
+  console.log('확인');
+  // location.href = ''; // 생성해야함
+  // profiles 배열에 추가되게 addProfile() 함수 사용
+}
+avatarsAddButton.addEventListener('click', addAvatar);
 
 //타잉 페이지로 이동
 function goToTaing(e) {
@@ -20,3 +53,4 @@ function goToTaing(e) {
 
   location.href = '/src/pages/taing/index.html';
 }
+avatarsContainer.addEventListener('click', goToTaing);
