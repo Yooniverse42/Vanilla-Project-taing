aaa65a95b436ed053f8b3b66b75dff1bf4924f86
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2024-12-01 23:55:38 +0900

    feat : 메인 페이지 이동 시 로컬스토리지에 currentProfile 값 저장

--- src/pages/profile_select/profile_select.js
+++ src/pages/profile_select/profile_select.js
@@ -1,56 +1,49 @@
-import '@//pages/profile_select/profile.scss';
+import '@/styles/pages/profile.scss';
 import '@/layout/footer';
 import { renderProfileItem } from '@/layout/profile_item';
-import { getNode, getNodes } from '@/library/getNode';
+import { getNode } from '@/library/getNode';
 renderProfileItem('main');
 
-const title = getNode('.header__title__container');
 const avatarsContainer = getNode('.profile__picture__container');
-const avatarsPictures = getNodes('.avatar__picture__container');
 const avatarsEditButton = getNode('.edit__button');
-const avatarsAddButton = getNode('.add__button');
+const dialog = getNode('.dialog__container');
+const dialogCancelButton = getNode('.dialog__exit__button');
+
+const { profiles } = JSON.parse(localStorage.getItem('user')).record;
 
 // 프로필 편집 (전체 페이지)
 function editPage() {
-  // 기존 프로필 아이템들을 지우고
-  avatarsContainer.innerHTML = '';
-
-  // 편집 모드로 다시 렌더링
-  renderProfileItem('edit');
-
-  title.innerHTML = `프로필 편집
-  <p>편집할 프로필을 선택해 주세요.</p>`;
-
-  avatarsPictures.forEach((avatar) => {
-    if (avatar.classList.contains('is--locked')) {
-      avatar.classList.remove('is--locked');
-      avatar.classList.add('is--edit');
-    } else {
-      avatar.classList.add('is--edit');
-    }
-  });
+  location.href = '/src/pages/profile_edit/index.html';
 }
 avatarsEditButton.addEventListener('click', editPage);
-
-// 프로필 추가
-function addAvatar() {
-  console.log('확인');
-  // location.href = ''; // 생성해야함
-  // profiles 배열에 추가되게 addProfile() 함수 사용
-}
-avatarsAddButton.addEventListener('click', addAvatar);
 
 //타잉 페이지로 이동
 function goToTaing(e) {
   const target = e.target;
-  console.log('target', target);
-  if (!target.matches('.avatar')) return;
+  const img = target.querySelector('img');
+  if (!img) return;
 
-  const imgSrc = target.children[1].src;
-  const name = target.children[2].textContent;
+  const imgSrc = img.src;
+  const name = img.alt.replace('의 프로필', '');
+  const currentProfile = profiles.find((item) => item.name === name);
+  const pw = currentProfile?.lockPassword || null;
 
-  localStorage.setItem('profileInfo', JSON.stringify({ name, imgSrc }));
-
-  location.href = '/src/pages/taing/index.html';
+  localStorage.setItem('currentProfile', JSON.stringify({ pw, imgSrc, name }));
 }
 avatarsContainer.addEventListener('click', goToTaing);
+
+// 잠금 설정 프로필
+// 비밀번호 입력
+const lockedProfile = getNode('.is--locked');
+lockedProfile.addEventListener('click', () => {
+  dialog.showModal();
+});
+
+// dialog 닫기
+dialogCancelButton.addEventListener('click', () => {
+  dialog.close();
+});
+
+dialog.addEventListener('cancel', () => {
+  dialog.close();
+});
