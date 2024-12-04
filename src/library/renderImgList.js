5089f0443165efa78127f263e13b1904e9c3a1cf
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2024-12-04 16:43:11 +0900

    refactor : 불필요 함수 삭제

--- src/library/renderImgList.js
+++ src/library/renderImgList.js
@@ -1,6 +1,5 @@
 import { insertLast } from '@/library/insert';
 import getPbImageURL from '@/api/getPbImageURL';
-import { getRecord } from '@/api/getRecords';
 
 export async function renderImgList(collection, categoryName, node) {
   const records = await collection;
@@ -19,47 +18,4 @@ export async function renderImgList(collection, categoryName, node) {
     `;
     insertLast(node, template);
   });
-}
-
-export async function renderProfileImg(profileNumber, avatarName, container) {
-  const user = JSON.parse(localStorage.getItem('user')).record.profiles;
-  const myProfileAvatar = user.find((item) => item.name == avatarName).avatar;
-  console.log(myProfileAvatar);
-
-  const records = await getRecord('profile', `name='profile${profileNumber}'`);
-  const item = records.items[0];
-
-  if (!item) return;
-
-  const template = `
-    <picture>
-      <source srcset="${getPbImageURL(item, 'tablet')}" media="(min-width: 768px) and (max-width: 1279px)" />
-      <source srcset="${getPbImageURL(item, 'desktop')}" media="(min-width: 1280px)"/>
-      <img 
-        class="avatar__picture"
-        src="${getPbImageURL(item)}"
-        alt="${avatarName}의 프로필" 
-      />
-    </picture>
-  `;
-
-  container.innerHTML = template;
-}
-
-export async function getProfileImg(
-  profileNumber,
-  breakpoint = 'mobile',
-  fileName = 'photo'
-) {
-  const records = await getRecord('profile', `name='profile${profileNumber}'`);
-  const item = records.items[0];
-
-  if (!item) return null;
-
-  if (breakpoint === 'mobile')
-    return `${import.meta.env.VITE_PB_API}/files/${item.collectionId}/${item.id}/${item[fileName][0]}`;
-  if (breakpoint === 'tablet')
-    return `${import.meta.env.VITE_PB_API}/files/${item.collectionId}/${item.id}/${item[fileName][1]}`;
-  if (breakpoint === 'desktop')
-    return `${import.meta.env.VITE_PB_API}/files/${item.collectionId}/${item.id}/${item[fileName][2]}`;
 }
