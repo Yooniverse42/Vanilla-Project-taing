ac998928df2395d646b53a8e32ff5cb3d80f4e38
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2024-12-13 17:09:10 +0900

    feat : 로딩스피너 웹컴포넌트로 구현

---
+++ src/components/loading.js
@@ -0,0 +1,57 @@
+import textCSS from '@/styles/components/loading.scss?inline';
+import gsap from 'gsap';
+
+const loadingTemplate = document.createElement('template');
+loadingTemplate.innerHTML = `
+  <style>
+    ${textCSS}
+  </style>
+  <div class="loading">
+    <div class="loading_wrapper">
+      <h2><slot name="text">loading...</slot></h2>
+      <div class="loading_inner"></div>
+    </div>
+  </div>
+`;
+
+export class Loading extends HTMLElement {
+  #spinnerAnimation;
+
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.appendChild(loadingTemplate.content.cloneNode(true));
+
+    const spinner = this.shadowRoot.querySelector('.loading_inner');
+    this.#spinnerAnimation = gsap.to(spinner, {
+      rotation: 360,
+      duration: 2,
+      repeat: -1,
+      ease: 'none',
+      paused: true,
+    });
+  }
+
+  static get observedAttributes() {
+    return ['text'];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === 'text' && newValue) {
+      const slot = this.shadowRoot.querySelector('slot[name="text"]');
+      slot.textContent = newValue;
+    }
+  }
+
+  show() {
+    this.classList.add('active');
+    this.#spinnerAnimation.play();
+  }
+
+  hide() {
+    this.classList.remove('active');
+    this.#spinnerAnimation.pause();
+  }
+}
+
+customElements.define('c-loading', Loading);
