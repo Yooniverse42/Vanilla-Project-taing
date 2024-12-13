cbd1d59da1dd9f43613b99da4cc5a05387f3e84c
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2024-12-13 15:02:56 +0900

    rename : 파일 위치 이동

--- src/pages/profile/profile_edit/profile_edit.js
+++ src/pages/profile/profile_edit/profile_edit.js
@@ -1,6 +1,6 @@
 import '@/styles/pages/profile.scss';
 import '@/layout/footer';
-import { renderProfileItem } from '@/layout/profile_item';
+import { renderProfileItem } from '@/pages/profile/profile_item';
 import { getNode } from '@/library/index';
 
 renderProfileItem('edit');
