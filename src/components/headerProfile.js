f81ea6a603b81e763f1da94f49667861b4ea9ec7
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2024-12-19 23:58:57 +0900

    feat : sweetAlert 도구 사용

--- src/components/headerProfile.js
+++ src/components/headerProfile.js
@@ -1,6 +1,7 @@
 import pb from '@/api/pocketbase';
 import { getStorage, deleteStorage } from '@/library/index';
 import textCSS from '@/styles/components/headerProfile.scss?inline';
+import { sweetConfirm } from '@/components/sweetAlert';
 
 const currentProfile = JSON.parse(localStorage.getItem('currentProfile'));
 
@@ -35,15 +36,21 @@ export class ProfileModal extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: 'open' });
-  }
-
-  connectedCallback() {
     this.shadowRoot.appendChild(profileTemplate.content.cloneNode(true));
     this.#dialog = this.shadowRoot.querySelector('dialog');
 
     this.logoutButton = this.shadowRoot.querySelector('.logout_button');
     this.deleteButton = this.shadowRoot.querySelector('.deleteID_button');
 
+    this.#dialog.addEventListener('click', (e) => {
+      if (e.target.closest('.profile_container')) {
+        return;
+      }
+      this.close();
+    });
+  }
+
+  connectedCallback() {
     this.logoutButton.addEventListener('click', this.handleLogout.bind(this));
     this.deleteButton.addEventListener(
       'click',
@@ -66,12 +73,20 @@ export class ProfileModal extends HTMLElement {
     const target = e.target.closest('button');
 
     if (target == e.currentTarget) {
-      if (!confirm('로그아웃 하시겠습니까?')) {
-        return;
-      }
-      deleteStorage('user');
-      deleteStorage('currentProfile');
-      window.location.href = '/';
+      sweetConfirm(
+        'warning',
+        '로그아웃 하시겠습니까?',
+        '로그아웃 시 메인 페이지로 이동합니다.',
+        '확인',
+        false,
+        '취소'
+      ).then((res) => {
+        if (res.isConrimed) {
+          deleteStorage('user');
+          deleteStorage('currentProfile');
+          window.location.href = '/';
+        }
+      });
     }
   }
 
@@ -79,16 +94,24 @@ export class ProfileModal extends HTMLElement {
     const target = e.target.closest('button');
 
     if (target == e.currentTarget) {
-      if (!confirm('회원 탈퇴를 진행 하시겠습니까?')) {
-        return;
-      }
-      let user = await getStorage('user');
-      await pb.collection('users').delete(user.record.id);
-      await pb.collection('profileinfo').delete(user.record.id);
-      deleteStorage('user');
-      deleteStorage('currentProfile');
-      alert('회원 탈퇴가 완료 되었습니다.');
-      location.href = '/';
+      sweetConfirm(
+        'warning',
+        '정말 탈퇴하시겠습니까?',
+        '탈퇴 시 계정 정보와 프로필이 모두 삭제되며, 이 작업은 되돌릴 수 없습니다.',
+        '확인',
+        false,
+        '취소'
+      ).then(async (res) => {
+        if (res.isConrimed) {
+          const user = await getStorage('user');
+          await pb.collection('users').delete(user.record.id);
+          await pb.collection('profileinfo').delete(user.record.id);
+          deleteStorage('user');
+          deleteStorage('currentProfile');
+          alert('회원 탈퇴가 완료 되었습니다.');
+          location.href = '/';
+        }
+      });
     }
   }
 }
