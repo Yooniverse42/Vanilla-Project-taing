a1a151c8005ab41c53b8127322b0c617ea6ee51e
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2024-12-20 00:27:56 +0900

    fix : dialog 닫을 때 접근성 이슈 해결

--- src/components/headerProfile.js
+++ src/components/headerProfile.js
@@ -64,29 +64,30 @@ export class ProfileModal extends HTMLElement {
 
   close() {
     this.#dialog.close();
-    this.getRootNode()
-      .host.shadowRoot.querySelector('.button_profile_open')
-      .focus();
   }
 
   handleLogout(e) {
     const target = e.target.closest('button');
 
     if (target == e.currentTarget) {
-      sweetConfirm(
-        'warning',
-        '로그아웃 하시겠습니까?',
-        '로그아웃 시 메인 페이지로 이동합니다.',
-        '확인',
-        false,
-        '취소'
-      ).then((res) => {
-        if (res.isConrimed) {
-          deleteStorage('user');
-          deleteStorage('currentProfile');
-          window.location.href = '/';
-        }
-      });
+      this.close();
+
+      if (target == e.currentTarget) {
+        sweetConfirm(
+          'warning',
+          '로그아웃 하시겠습니까?',
+          '로그아웃 시 메인 페이지로 이동합니다.',
+          '확인',
+          false,
+          '취소'
+        ).then((res) => {
+          if (res.isConrimed) {
+            deleteStorage('user');
+            deleteStorage('currentProfile');
+            window.location.href = '/';
+          }
+        });
+      }
     }
   }
 
@@ -94,24 +95,28 @@ export class ProfileModal extends HTMLElement {
     const target = e.target.closest('button');
 
     if (target == e.currentTarget) {
-      sweetConfirm(
-        'warning',
-        '정말 탈퇴하시겠습니까?',
-        '탈퇴 시 계정 정보와 프로필이 모두 삭제되며, 이 작업은 되돌릴 수 없습니다.',
-        '확인',
-        false,
-        '취소'
-      ).then(async (res) => {
-        if (res.isConrimed) {
-          const user = await getStorage('user');
-          await pb.collection('users').delete(user.record.id);
-          await pb.collection('profileinfo').delete(user.record.id);
-          deleteStorage('user');
-          deleteStorage('currentProfile');
-          alert('회원 탈퇴가 완료 되었습니다.');
-          location.href = '/';
-        }
-      });
+      this.close();
+
+      if (target == e.currentTarget) {
+        sweetConfirm(
+          'warning',
+          '정말 탈퇴하시겠습니까?',
+          '탈퇴 시 계정 정보와 프로필이 모두 삭제되며,<br />이 작업은 되돌릴 수 없습니다.',
+          '확인',
+          false,
+          '취소'
+        ).then(async (res) => {
+          if (res.isConrimed) {
+            const user = await getStorage('user');
+            await pb.collection('users').delete(user.record.id);
+            await pb.collection('profileinfo').delete(user.record.id);
+            deleteStorage('user');
+            deleteStorage('currentProfile');
+            alert('회원 탈퇴가 완료 되었습니다.');
+            location.href = '/';
+          }
+        });
+      }
     }
   }
 }
