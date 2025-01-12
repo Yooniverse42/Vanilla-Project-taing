d51723817d7daa5cbd95f08f78d9debf62cd5780
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2025-01-12 16:37:20 +0900

    feat : 키보드 포커스를 위해 a 태그 추가 및 이미지 랭킹별 정렬 구현

--- src/library/renderImgList.js
+++ src/library/renderImgList.js
@@ -2,9 +2,18 @@ import { insertLast } from '@/library/insert';
 import getPbImageURL from '@/api/getPbImageURL';
 import { getRecords } from '@/api/getRecords';
 
-export async function renderImgList(collection, categoryName, node) {
+export async function renderImgList(
+  collection,
+  categoryName,
+  node,
+  sortField = null
+) {
   const records = await collection;
   let record = records.filter((item) => item.category == categoryName);
+
+  if (sortField) {
+    record = record.sort((a, b) => a[sortField] - b[sortField]);
+  }
 
   record.forEach((item) => {
     if (item.photo.length === 1) {
@@ -17,13 +26,15 @@ export async function renderImgList(collection, categoryName, node) {
     } else {
       const template = `
       <li class="swiper-slide">
-        <picture>
-          <source srcset="${getPbImageURL(item, 'tablet')}" media="(min-width: 768px) and (max-width: 1279px)" />
-          <source srcset="${getPbImageURL(item, 'desktop')}" media="(min-width: 1280px)"/>
-          <img src="${getPbImageURL(item)}" alt="${item.title}" />
-        </picture>
+        <a href="javascript:void(0)">
+          <picture>
+            <source srcset="${getPbImageURL(item, 'tablet')}" media="(min-width: 768px) and (max-width: 1279px)" />
+            <source srcset="${getPbImageURL(item, 'desktop')}" media="(min-width: 1280px)"/>
+            <img src="${getPbImageURL(item)}" alt="${item.title}" />
+          </picture>
+        </a>
       </li>
-    `;
+      `;
       insertLast(node, template);
     }
   });
@@ -41,17 +52,16 @@ export async function renderSearchList(
 
   records.forEach((item) => {
     const template = `
-    <li>
-      <figure>
+    <li class="swiper-slide">
+      <a href="javascript:void(0)">
         <picture>
           <source srcset="${getPbImageURL(item, 'tablet')}" media="(min-width: 768px) and (max-width: 1279px)" />
           <source srcset="${getPbImageURL(item, 'desktop')}" media="(min-width: 1280px)"/>
           <img src="${getPbImageURL(item)}" alt="${item.title}" />
         </picture>
-        <figcaption>${item.title}</figcaption>
-      </figure>
+      </a>
     </li>
-  `;
+    `;
     insertLast(node, template);
   });
 }
